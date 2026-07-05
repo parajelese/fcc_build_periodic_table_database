62002b7a1ba18f71f16922a6914948e44681848d
parajelese <parajelese.edu@gmail.com>
2026-07-05 12:02:22 -0400

    test:testing results where ok.

--- element.sh
+++ element.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#periodic table database testing
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo -e "\n~~Welcome to Periodic Table Database~~\n"
 if [[ -z $1 ]]
